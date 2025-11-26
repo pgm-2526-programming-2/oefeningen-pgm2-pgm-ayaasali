86fb1f10d993c1b7ccb524f86299dd9e0c9d003a
pgm-ayaasali <ayaasali@student.arteveldehs.be>
2025-11-26 13:05:51 +0100

    function

---
+++ week-1/les-1/functions.js
@@ -0,0 +1,19 @@
+function sum(a, b) {
+    return a + b
+}
+
+
+const name = 'aya'
+const divistion = () => { }
+const multiplier = function () { }
+
+function selectRandomStudent(arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
+}
+
+const age = 21
+const students = ['yme', 'arne', 'mart']
+
+console.log(sum(age, 12))
+console.log(20)
+console.log(selectRandomStudent(students))
